537a3291c6496dff3f07391c4ca2db012c0e449e
DiegoGCH <64449084+DiegoGCh@users.noreply.github.com>
2021-09-13 23:22:21 -0500

    comit1

--- stack.h
+++ stack.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef STACKARR_H
 #define STACKARR_H
 
@@ -26,35 +25,4 @@ class StackArray: public CircularArray<T> {
           return this->to_string();
         };
 };
-#endif
-=======
-#ifndef STACKARR_H
-#define STACKARR_H
-
-#include "circulararray.h"
-
-template <typename T>
-class StackArray: public CircularArray<T> { 
-    public:
-        StackArray<T>(): CircularArray<T>(0){};
-
-        StackArray<T>(int _capacity): CircularArray<T>(_capacity){};
-
-        ~StackArray(){
-        };
-
-        void stack_push_back(T data){
-          this->push_back(data);
-        };
-
-        T stack_pop_back(){
-          return this->pop_back();
-        };
-
-        string stack_to_string(){
-          return this->to_string();
-        };
-};
-
->>>>>>> d37b28edc9cf68daac4b724a24bf8a49372c1734
 #endif
